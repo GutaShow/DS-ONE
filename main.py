86c4a25c43b84e26633a456c64665b7cb0c53e03
GutaShow <108829708+GutaShow@users.noreply.github.com>
2025-09-22 10:28:10 -0300

    V0.04
    Foi adicionada a logo no projeto e um comeco do que sera a tela inicial

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import pygame
 pygame.init()
 
 # Icone Do jogo
-logo=pygame.image.load('img/DS-One_Logo.png')
+logo=pygame.image.load('img/DS-One_Logomini.png')
 pygame.display.set_icon(logo)
+
+# Imagens do Jogo
+fundo=pygame.image.load('img/fundo.jpg') # Fundo da Fase 1
+player=pygame.image.load('img/nave.png') # Jogador
+fundoini=pygame.image.load('img/fundoini.gif') # Fundo da tela inicial
+logocomp=pygame.image.load('img/DS-One_Logo.png') # Logo da tela inicial
 
 # Definicao da janela
 janela=pygame.display.set_mode([1200, 611])
 pygame.display.set_caption('DS-ONE: Multiverso do Caos Acadêmico') # Titulo da janela
-fundo=pygame.image.load('img/fundo.jpg')
-player=pygame.image.load('img/nave.png')
 
 # Posicao do jogador
 pos_yplayer=300 # Cima/Baixo
@@ -17,47 +21,72 @@ pos_xplayer=400 # Esquerda/Direita
 vel_player=0.5 # Velocidade da nave
 
 # VARIAVEIS
+game=True
 loop=True
+inicio= True
 
 
-
-
-
-# Loop do Jogo
-while loop:
+while game:
     # Botao de fechar a janela
     for events in pygame.event.get():
-        if events.type==pygame.QUIT:
-            loop=False
+            if events.type==pygame.QUIT:
+                game=False
+                inicio=False
+                loop=False
     
-    # indentifica quais teclas estao sendo pressionadas 
-    teclas=pygame.key.get_pressed()    
-    
-    # Movimentacao da nave       
-    if teclas[pygame.K_UP]: # Cima
-        pos_yplayer-=vel_player
-    if teclas[pygame.K_DOWN]: # Baixo
-        pos_yplayer+=vel_player
-    if teclas[pygame.K_LEFT]: # Esquerda
-        pos_xplayer-=vel_player
-    if teclas[pygame.K_RIGHT]: # Direita
-        pos_xplayer+=vel_player 
-    
-    # Bordas do Jogo
-    if pos_yplayer <= -7:
-        pos_yplayer=-7
-    if pos_yplayer >= 548:
-        pos_yplayer=548
-    if pos_xplayer <= -20:
-        pos_xplayer=-20
-    if pos_xplayer >= 1082:
-        pos_xplayer=1082
-            
-    # inserindo imagem na janela      
-    janela.blit(fundo, (0, 0))
-    janela.blit(player, (pos_xplayer, pos_yplayer))
-    
-    
-    # Atualizacao da tela
-    pygame.display.update()
-    
+    # Loop da tela inicial
+    while inicio:
+        # Botao de fechar a janela
+        for events in pygame.event.get():
+            if events.type==pygame.QUIT:
+                inicio=False
+                
+        
+        # Chamar a imagem de Fundo
+        janela.blit(fundoini, (0, 0))
+        janela.blit(logocomp, (200, 200))
+        
+        # Atualizacao da tela
+        pygame.display.update()
+
+
+    # Loop do Jogo
+    while loop:
+        # Botao de fechar a janela
+        for events in pygame.event.get():
+            if events.type==pygame.QUIT:
+                loop=False
+                game=False
+                inicio=False
+        
+        # indentifica quais teclas estao sendo pressionadas 
+        teclas=pygame.key.get_pressed()    
+        
+        # Movimentacao da nave       
+        if teclas[pygame.K_UP] or teclas[pygame.K_w]: # Cima
+            pos_yplayer-=vel_player
+        if teclas[pygame.K_DOWN] or teclas[pygame.K_s]: # Baixo
+            pos_yplayer+=vel_player
+        if teclas[pygame.K_LEFT] or teclas[pygame.K_a]: # Esquerda
+            pos_xplayer-=vel_player
+        if teclas[pygame.K_RIGHT] or teclas[pygame.K_d]: # Direita
+            pos_xplayer+=vel_player 
+        
+        # Bordas do Jogo
+        if pos_yplayer <= -7:
+            pos_yplayer=-7
+        if pos_yplayer >= 548:
+            pos_yplayer=548
+        if pos_xplayer <= -20:
+            pos_xplayer=-20
+        if pos_xplayer >= 1082:
+            pos_xplayer=1082
+                
+        # inserindo imagem na janela      
+        janela.blit(fundo, (0, 0))
+        janela.blit(player, (pos_xplayer, pos_yplayer))
+        
+        
+        # Atualizacao da tela
+        pygame.display.update()
+        
