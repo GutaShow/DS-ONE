983da91d96856c3b126ece25dc1bb3f0cf426604
GutaShow <108829708+GutaShow@users.noreply.github.com>
2025-09-21 23:23:05 -0300

    V0.02
    Foi adicionado a nave player e sua movimentacao com as bordas limitando sua movimentacao

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
 import pygame
 
 # Definicao da janela
-janela=pygame.display.set_mode([800, 600])
+janela=pygame.display.set_mode([1200, 611])
 pygame.display.set_caption('DS-ONE: Multiverso do Caos Acadêmico') # Titulo da janela
+fundo=pygame.image.load('img/fundo.jpg')
+player=pygame.image.load('img/nave.png')
+
+# Posicao do jogador
+pos_yplayer=300 # Cima/Baixo
+pos_xplayer=400 # Esquerda/Direita
+vel_player=0.5 # Velocidade da nave
 
 # VARIAVEIS
 loop=True
 
+# Loop do Jogo
 while loop:
+    # Botao de fechar a janela
     for events in pygame.event.get():
         if events.type==pygame.QUIT:
             loop=False
+    
+    # indentifica quais teclas estao sendo pressionadas 
+    teclas=pygame.key.get_pressed()    
+    
+    # Movimentacao da nave       
+    if teclas[pygame.K_UP]: # Cima
+        pos_yplayer-=vel_player
+    if teclas[pygame.K_DOWN]: # Baixo
+        pos_yplayer+=vel_player
+    if teclas[pygame.K_LEFT]: # Esquerda
+        pos_xplayer-=vel_player
+    if teclas[pygame.K_RIGHT]: # Direita
+        pos_xplayer+=vel_player 
+    
+    # Bordas do Jogo
+    if pos_yplayer <= -7:
+        pos_yplayer=-7
+    if pos_yplayer >= 548:
+        pos_yplayer=548
+    if pos_xplayer <= -20:
+        pos_xplayer=-20
+    if pos_xplayer >= 1082:
+        pos_xplayer=1082
+            
+    # inserindo imagem na janela      
+    janela.blit(fundo, (0, 0))
+    janela.blit(player, (pos_xplayer, pos_yplayer))
+    
+    
+    # Atualizacao da tela
     pygame.display.update()
+    
